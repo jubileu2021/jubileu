ec5064d0fee0e95de8fa8c95bd6fffbb477328c3
jubileu2021 <pubg968mss@gmail.com>
2021-02-06 18:23:34 -0300

    First commit

---
+++ database/menu/xp.js
@@ -0,0 +1,24 @@
+const xp = (prefix, botName, ownerName) => {
+        return `
+「 *${botName}* 」
+
+◪ *INFO*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Creator: ${Mendigo}
+  ❏ Version: 0.0.4
+◪ *ABOUT*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+◪ *XP*
+  │
+  ├─ ❏ ${prefix}leveling
+  ├─ ❏ ${prefix}level
+  └─ ❏ ${prefix}mining
+--------------------------------
+Note: Kamu kkkkkkk.`
+}
+exports.xp = xp
